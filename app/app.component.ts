d953ced40d2a4cfb5562df2e50f6bb60297779f5
Pavel Sumarokov <psumarokov@thumbtack.net>
2016-03-20 19:45:25 +0600

    Add router to the main app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,22 @@
 import {Component} from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
+import {CarService} from './car/car.service';
 import {CarListComponent} from './car/car-list.component';
 
 @Component({
     selector: 'base-app',
     templateUrl: 'app/app.component.html',
-    directives: [CarListComponent]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [CarService, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    {
+        path: '/cars',
+        name: 'Cars',
+        component: CarListComponent
+    }
+])
 export class AppComponent {
     public title = 'Tour of Cars';
 }
--- app/car/car.service.ts
+++ app/car/car.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from 'angular2/core';
-import {Car} from './car';
 import {CARS} from './mock-cars';
 
 @Injectable()
